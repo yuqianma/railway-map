b8c9728c6523b2fa2923a7bfde365c1eeeccef4f
Yuqian Ma <mayq0422@gmail.com>
2022-09-19 00:18:45 +0800

    feat: implement basic time range slider function

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,16 +1,29 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
+import { observer } from "mobx-react-lite";
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { useStore } from "../../useStore";
 import "./index.css";
 
-type TimelineProps = {
-	onTimeChange?: (time: number) => void;
-};
+export const Timeline = observer(() => {
+	const store = useStore();
+	const initTimeRangeRef = useRef(store.timeRange);
 
-export const Timeline = ({
-	onTimeChange
-}: TimelineProps) => {
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    store.setTimeRange(newValue as number[]);
+  };
+
 	return (
 		<div className="timeline">
-			<input type="range"></input>
+			<Box sx={{ width: "98%" }}>
+				<Slider
+					min={initTimeRangeRef.current[0]}
+					max={initTimeRangeRef.current[1]}
+					value={store.timeRange}
+					onChange={handleChange}
+					valueLabelDisplay="auto"
+				/>
+			</Box>
 		</div>
 	);
-};
+});
